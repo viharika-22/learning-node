81f2b69991532918c00442e14e2b24db949ba34a
viharika-22 <55423663+viharika-22@users.noreply.github.com>
2025-07-17 23:38:01 +0530

    Update intro.js

--- Day01_Intro/hands-on/intro.js
+++ Day01_Intro/hands-on/intro.js
@@ -1,1 +1,12 @@
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
 
+const user = os.userInfo()
+const uptime = os.uptime()
+const freeMem = os.freemem()
+
+fs.writeFileSync('log.txt',`User: ${user.username}\nUptime: ${uptime}\nFree Memory: ${freeMem}`)
+
+path.join(__dirname, 'logs','log.txt')
+console.log("System info logged successfully ✅")
